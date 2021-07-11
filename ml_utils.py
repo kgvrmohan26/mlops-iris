bea46514d48db8de69aad6433a421eaeaa87eb37
kgvrmohan26 <kgvrmohan26@gmail.com>
2021-07-11 01:35:26 -0400

    [Update] README and add name

--- ml_utils.py
+++ ml_utils.py
@@ -2,9 +2,14 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 # define a Gaussain NB classifier
 clf = GaussianNB()
+
+#define a Linear classifier
+clf_Lreg = LogisticRegression(penalty='l2',C=1.0, max_iter=10000)
+
 
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
@@ -12,16 +17,32 @@ r_classes = {y: x for x, y in classes.items()}
 
 # function to train and load the model during startup
 def load_model():
+    
+    global clf
+    global clf_Lreg
+    
     # load the dataset from the official sklearn datasets
     X, y = datasets.load_iris(return_X_y=True)
 
     # do the test-train split and train the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf.fit(X_train, y_train)
+    clf.fit(X_train, y_train) # Fit Gaussain classifier
+    
+    clf_Lreg.fit(X_train, y_train) # Fit Linear classifier
 
     # calculate the print the accuracy score
-    acc = accuracy_score(y_test, clf.predict(X_test))
-    print(f"Model trained with accuracy: {round(acc, 3)}")
+    acc = accuracy_score(y_test, clf.predict(X_test))  # Gaussain classifier accuracy
+    acc_Lreg = accuracy_score(y_test, clf_Lreg.predict(X_test))  # Linear classifier accuracy
+    print(f"Model GaussianNB classifier trained with accuracy: {round(acc, 3)}")
+    print(f"Model LogisticRegression classifier trained with accuracy: {round(acc_Lreg, 3)}")
+    if (acc < acc_Lreg): #if LogisticRegression accuracy better than GaussianNB
+       clf=clf_Lreg #Assign LogisticRegression classifier to clf
+       print ('LogisticRegression got better accuracy')
+       print('classifier used for prediction is',clf)
+    else: #if GaussianNB is better/equal than/to LogisticRegression
+       print ('GaussianNB got better/equal accuracy')
+       print('classifier used for prediction is',clf)#clf contain GaussianNB classifier as assigned initially
+    
 
 
 # function to predict the flower using the model
@@ -29,7 +50,7 @@ def predict(query_data):
     x = list(query_data.dict().values())
     prediction = clf.predict([x])[0]
     print(f"Model prediction: {classes[prediction]}")
-    return classes[prediction]
+    return (classes[prediction])
 
 # function to retrain the model as part of the feedback loop
 def retrain(data):
--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+import datetime;
 
 # test to check the correct functioning of the /ping route
 def test_ping():
@@ -23,4 +24,34 @@ def test_pred_virginica():
         response = client.post("/predict_flower", json=payload)
         # asserting the correct response is received
         assert response.status_code == 200
-        assert response.json() == {"flower_class": "Iris Virginica"}
+        assert response.json() == {"flower_class": "Iris Virginica","Time_Stamp":datetime.datetime.now().strftime("%m/%d/%Y, %H:%M")}
+       
+def test_pred_setosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 5.1,
+        "sepal_width": 3.5,
+        "petal_length": 1.4,
+        "petal_width": 0.2,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Setosa","Time_Stamp":datetime.datetime.now().strftime("%m/%d/%Y, %H:%M")}
+
+
+def test_pred_Versicolour():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 7.0,
+        "sepal_width": 3.2,
+        "petal_length": 4.7,
+        "petal_width": 1.4,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+        # asserting the correct response is received
+        assert response.status_code == 200
+        assert response.json() == {"flower_class": "Iris Versicolour","Time_Stamp":datetime.datetime.now().strftime("%m/%d/%Y, %H:%M")}
+
